b93c17bb61d6e9a68bab61372e2bbfa2b79d0950
omar-tarek-abdelfattah <omartarek77124@gmail.com>
2026-09-06 03:17:49 +0300

    fixed el 3ak

--- next.config.ts
+++ next.config.ts
@@ -2,10 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  i18n: {
-    locales: ['en', 'ar'],
-    defaultLocale: 'en',
-  },
+
 };
 
 export default nextConfig;
